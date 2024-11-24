81c1a0767745a678b10c03feb446cba50f7bffab
DSKottachchi <dskottachchi@outlook.com>
2024-11-24 17:14:27 +0100

    chore: adding type definitions

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export type Restaurant = {
+    id: string;
+    name: string;
+    rating: number;
+    filter_ids: string[];
+    image_url: string;
+    delivery_time_minutes: number;
+    price_range_id: string;
+};
+
+export type Filter = {
+    id: string;
+    name: string;
+    image_url: string;
+};
+
+export type PriceRange = {
+    id: string;
+    range: string;
+};
