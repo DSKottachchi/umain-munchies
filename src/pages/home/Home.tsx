ddf02132dc0414de517e2e12611c055405c3b40c
DSKottachchi <dskottachchi@outlook.com>
2024-11-24 17:15:09 +0100

    feat: adding filter mapping UI

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,30 +1,17 @@
 import { useEffect, useState } from "react";
 import Card from "../../components/ui/Card";
 import { Categories } from '../../utils/data.ts';
-
-type Restaurant = {
-    id: string;
-    name: string;
-    rating: number;
-    filter_ids: string[];
-    image_url: string;
-    delivery_time_minutes: number;
-    price_range_id: string;
-};
-
-type Filter = {
-    id: string;
-    name: string;
-    image_url: string;
-};
+import { Restaurant, Filter, PriceRange } from '../../types/index.ts';
 
 const Home = () => {
     const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
     const [filters, setFilters] = useState<Filter[]>([]);
+    const [priceRanges, setPriceRange] = useState<PriceRange[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
+        fetchPriceRange();
         fetchRestuarants();
         fetchFilters();
     }, []);
@@ -68,6 +55,25 @@ const Home = () => {
         }
     };
 
+    const fetchPriceRange = async () => {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/price-range`);
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
+            }
+            const result = await response.json();
+            setPriceRange(result);
+        } catch (err) {
+            if (err instanceof Error) {
+                setError(err.message);
+            } else {
+                setError("An unexpected error occurred");
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
+
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error}</p>;
 
@@ -84,8 +90,45 @@ const Home = () => {
             <div className="flex">
                 <aside className="p-6 bg-white border border-gray-200 rounded-lg w-64 hidden md:block mr-4">
                     <div className="mb-6">Filter</div>
-                    <div className="mb-6">Food Category</div>
-                    
+                    <div className="mb-6 uppercase text-slate-300">Food Category</div>
+                    <div className="mb-4">
+                        {
+                            filters.map((filter) => (
+                                <button
+                                    className={`inline-flex items-center w-fit h-fit p-2 mr-2 mb-2 gap-2 rounded-[8px] border-[0.6px]`}
+                                >
+                                    <span className="text-sm font-medium text-gray-800">{filter.name}</span>
+                                </button>
+                            ))
+                        }
+                    </div>
+
+
+                    <div className="mb-6 uppercase text-slate-300">Delivery Time</div>
+                    <div className="mb-4">
+                        {
+                            filters.map((filter) => (
+                                <button
+                                    className={`inline-flex items-center w-fit h-fit p-2 mr-2 mb-2 gap-2 rounded-[8px] border-[0.6px]`}
+                                >
+                                    <span className="text-sm font-medium text-gray-800">{filter.name}</span>
+                                </button>
+                            ))
+                        }
+                    </div>
+
+                    <div className="mb-6 uppercase text-slate-300">Price Range</div>
+                    <div className="mb-4">
+                        {
+                            priceRanges.map((priceRange) => (
+                                <button
+                                    className={`inline-flex items-center w-fit h-fit p-2 mr-2 mb-2 gap-2 rounded-[8px] border-[0.6px]`}
+                                >
+                                    <span className="text-sm font-medium text-gray-800">{priceRange.range}</span>
+                                </button>
+                            ))
+                        }
+                    </div>
                 </aside>
 
                 <div className="flex-1">
